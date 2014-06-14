cb1d616d7761152dfa96704f8f00af54abc2c518
Sina Samangooei <sinjax@gmail.com>
2014-06-14 08:57:49 +0000

    made debug mode default

--- homepi/__main__.py
+++ homepi/__main__.py
@@ -2,4 +2,5 @@ import sys, os
 sys.path.append(os.getcwd())
 from homepi import app
 
-app.run(host='0.0.0.0', port=7171, debug=True)
+app.run(host='0.0.0.0', port=7171, debug=app.config['DEBUG'])
+
